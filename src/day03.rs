844e23d50fc3933d2c03dc6c3ea79ac97013b79a
Edwin Scharfe <19590601+HippieHosenHustler@users.noreply.github.com>
2025-12-03 18:26:11 +0100

    day 3 p2

--- src/day03.rs
+++ src/day03.rs
@@ -2,18 +2,24 @@ use crate::file_helpers::read_lines;
 
 pub fn solve(input_file: &str) {
     let mut total_joltage = 0;
+    let mut total_overdrive_joltage: i128 = 0;
 
     let lines = read_lines(input_file).expect("Could not read lines from file");
 
     for line in lines {
         let line = line.expect("Could not read line from file");
-        total_joltage += calculate_maximum_joltage(line);
+        total_joltage += calculate_maximum_joltage(&line);
+        total_overdrive_joltage += calculate_overdrive_joltage(&line);
     }
 
     println!("Total maximum joltage (part 1 solution): {}", total_joltage);
+    println!(
+        "Total overdrive joltage (part 2 solution): {}",
+        total_overdrive_joltage
+    );
 }
 
-fn calculate_maximum_joltage(bank: String) -> i32 {
+fn calculate_maximum_joltage(bank: &String) -> i32 {
     let batteries = bank
         .chars()
         .map(|c| c.to_digit(10).unwrap() as i32)
@@ -38,4 +44,42 @@ fn calculate_maximum_joltage(bank: String) -> i32 {
 
     //combine the two digits into one number
     return first_val * 10 + second_val;
+}
+
+fn calculate_overdrive_joltage(bank: &String) -> i128 {
+    let batteries = bank
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as i128)
+        .collect::<Vec<i128>>();
+
+    let mut digits: Vec<i128> = vec![];
+
+    let mut starting_index: usize = 0;
+
+    for i in (0..=11).rev() {
+        let mut current_largest = 0;
+        let mut largest_index = starting_index;
+
+        for (idx, battery) in batteries[starting_index..(batteries.len() - i)]
+            .iter()
+            .enumerate()
+        {
+            if battery > &current_largest {
+                current_largest = battery.clone();
+                largest_index = starting_index + idx;
+            }
+        }
+
+        digits.push(current_largest);
+        starting_index = largest_index + 1;
+    }
+
+    // combine all the digits into one number
+    let mut result: i128 = 0;
+
+    for i in 0..digits.len() {
+        result += digits[i] * 10_i128.pow((digits.len() - 1 - i) as u32);
+    }
+
+    result
 }
